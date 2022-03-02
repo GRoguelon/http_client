64c0ce028badceac1db80c893fd889c25166507a
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2022-03-01 19:23:21 -0600

    Exported HttpMethod

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 export { Client as HttpClient } from "./src/client.ts";
-export { Request as HttpRequest } from "./src/request.ts";
+export { Method as HttpMethod, Request as HttpRequest } from "./src/request.ts";
 export {
   Response as HttpResponse,
   Status as HttpStatus,
